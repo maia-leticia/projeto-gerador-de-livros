1999dbd4605a4b673047d189fb9ca991a4e355ac
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-05-27 19:42:42 -0300

    update Header
    header responsivo com menu funcional

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import menuVector from '@/../public/img/Vector.png'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Header(){
+    const [openMenu, setOpenMenu] = useState(false);
+
+    useEffect(()=>{
+        const handleResize=()=>{
+            if(window.innerWidth>=640){
+                setOpenMenu(false);
+            }
+        }
+        window.addEventListener('resize',handleResize);
+        return ()=> window.removeEventListener('resize',handleResize)
+    },[])
+    
     return(
+        
     <header className='bg-[#F3F2EC] '>
         <div className='flex justify-around items-center p-10 '>
             <h1><span className='font-bold text-[46px]'>BOOK</span>saw</h1>
@@ -15,11 +28,12 @@ export default function Header(){
                 <li className='hover:text-[#74642F]'><Link href="#">Quote</Link></li>
                 <li className='hover:text-[#74642F]'><Link href="#">Contato</Link></li>
             </ul>
-            <div className='sm:hidden cursor-pointer'>
+            <div className='sm:hidden cursor-pointer' onClick={()=>setOpenMenu(!openMenu)}>
                 <Image src={menuVector} alt='botao menu'/>
             </div>
         </div>
-        <div >
+        {openMenu && (
+             <div className='sm:hidden'>
             <ul className=' text-[16px] w-full'>
                 <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Home</Link></li>
                 <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Em alta</Link></li>
@@ -27,6 +41,7 @@ export default function Header(){
                 <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Contato</Link></li>
             </ul>
         </div>
+        )}
     </header>
     )
    
