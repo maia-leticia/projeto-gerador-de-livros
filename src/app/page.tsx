fef73852df4f21aa2229b6f1f27cb35d47f80a5a
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-05-28 20:16:43 -0300

    Editei a pagina principal
    criei o bloco 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Header from "@/components/Header"
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import Header from "@/components/Header";
+import book from "@/../public/img/book.png"
+
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+    <main>
+      <Header/>
+      <div className='bg-[#F3F2EC] flex items-center justify-center'>
+        
+        <div className='flex items-center justify-center gap-15'>
+          
+          <div className='w-[25vw] '>
+            <h2 className='text-[4vw]'>Lorem Ipsum</h2>
+            <p className='text-[1vw] pt-[0.8vw] pb-[4vw]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro molestiae a id culpa corporis non eius, facere molestias vero cum. Cumque dicta voluptatem nesciunt doloremque veritatis explicabo eius quo numquam.</p>
+            <div className='w-[10vw] border border-[#C0C0C0] text-[1vw] text-center'>Botao</div>
+          </div>
+
+          <div className='w-[25vw] hidden sm:flex'>
+            <Image src={book} alt='livro-representativo'/>
+            <div className=''></div>
+          </div>
+        
+        </div>
+
+      </div>
+    </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,13 +16,13 @@ export default function Header(){
         window.addEventListener('resize',handleResize);
         return ()=> window.removeEventListener('resize',handleResize)
     },[])
-    
+
     return(
         
     <header className='bg-[#F3F2EC] '>
         <div className='flex justify-around items-center p-10 '>
-            <h1><span className='font-bold text-[46px]'>BOOK</span>saw</h1>
-            <ul className='hidden sm:flex gap-10 text-[16px] '>
+            <h1><span className='font-bold text-[3vw]'>BOOK</span>saw</h1>
+            <ul className='hidden sm:flex gap-10 text-[1vw] '>
                 <li className='hover:text-[#74642F]'><Link href="#">Home</Link></li>
                 <li className='hover:text-[#74642F]'><Link href="#">Em alta</Link></li>
                 <li className='hover:text-[#74642F]'><Link href="#">Quote</Link></li>
@@ -34,15 +34,14 @@ export default function Header(){
         </div>
         {openMenu && (
              <div className='sm:hidden'>
-            <ul className=' text-[16px] w-full'>
-                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Home</Link></li>
-                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Em alta</Link></li>
-                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Quote</Link></li>
-                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-5 border text-center'><Link href="#">Contato</Link></li>
+            <ul className=' text-[1vw] w-full'>
+                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-[1vw] border text-center'><Link href="#">Home</Link></li>
+                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-[1vw] border text-center'><Link href="#">Em alta</Link></li>
+                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-[1vw] border text-center'><Link href="#">Quote</Link></li>
+                <li className='hover:bg-[#74642F] hover:text-[#F3F2EC] p-[1vw] border text-center'><Link href="#">Contato</Link></li>
             </ul>
         </div>
         )}
     </header>
     )
-   
 }
